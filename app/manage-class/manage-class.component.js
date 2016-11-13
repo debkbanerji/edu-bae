efd64a074746448ae6302dd8517facdc918adca2
Deb Banerji <debkbanerji@gmail.com>
2016-11-12 21:07:18 -0500

    Getting Swag Questions

--- app/manage-class/manage-class.component.js
+++ app/manage-class/manage-class.component.js
@@ -11,6 +11,7 @@ angular.module('manageClass').component('manageClass', {
         self.questionsObject = $firebaseObject(self.questionsRef);
 
         self.categories = [];
+        self.questions = [];
         // putting a console.log here won't work due to the asynchronous call
 
         // handle loading of categories
@@ -30,9 +31,28 @@ angular.module('manageClass').component('manageClass', {
         //         console.log("Error:", error);
         //     });
 
-        self.categoriesRef.on('child_added', function(childSnapshot, prevChildKey) {
+        self.categoriesRef.on('child_added', function (categorySnapshot, prevChildKey) {
+            self.categories.push(categorySnapshot.getKey());
+
+            // add listener for getting appropriate questions
+            self.questionsRef.child(categorySnapshot.getKey()).on('child_added', function (questionSnapshot, prevChildKey) {
+                console.log("question", questionSnapshot.getKey());
+                self.questions.push({
+                    "text:": questionSnapshot.getKey(),
+                    "category": categorySnapshot.getKey()
+                });
+            });
+
+            // TODO: add listener for removing appropriate questions
+        });
+
+        self.categoriesRef.on('child_removed', function (childSnapshot, prevChildKey) {
             // console.log("category child", childSnapshot.getKey());
-            self.categories.push(childSnapshot.getKey());
+            var index = self.categories.indexOf(childSnapshot.getKey());
+            if (index > -1) {
+                self.categories.splice(index, 1);
+            }
+            console.log("Removed child");
         });
     }]
 });
