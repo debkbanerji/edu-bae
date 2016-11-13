f2e0822295ba2885bb6ea1e7cd3297ebafa5f053
Deb Banerji <debkbanerji@gmail.com>
2016-11-12 19:43:38 -0500

    Swag Pulling From Firebase

--- app/manage-class/manage-class.component.js
+++ app/manage-class/manage-class.component.js
@@ -1,7 +1,22 @@
 angular.module('manageClass').component('manageClass', {
     templateUrl: 'manage-class/manage-class.template.html',
 
-    controller: [function manageClassController() {
+    controller: ['$firebaseObject', function manageClassController($firebaseObject) {
         var self = this;
+        self.classRef = firebase.database().ref().child("root/debug/class");
+        self.categoriesRef = self.classRef.child("categories");
+        self.questionssRef = self.classRef.child("questions");
+
+        self.categories = $firebaseObject(self.categoriesRef);
+        // putting a console.log here won't work due to the asynchronous call
+        // self.puzzles = $firebaseArray(self.puzzlesRef);
+
+        self.categories.$loaded()
+            .then(function (x) {
+                console.log("TEST", x);
+            })
+            .catch(function (error) {
+                console.log("Error:", error);
+            });
     }]
 });
